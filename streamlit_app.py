964bfea26725cc65a60c7133a8621d4231a07874
moomoh <34252092+moomoh@users.noreply.github.com>
2023-08-16 20:20:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ redirect_uri = url
 
 
 if __name__ == '__main__':
-    st.title("Streamlit Oauth Login")
+    # st.title("Streamlit Oauth Login")
     st.write(get_login_str(), unsafe_allow_html=True)
         
     if st.button("display user"):  
