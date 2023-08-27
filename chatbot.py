57c00ecbc1c9afeb41a91c560ecf2ff5ca57f582
moomoh <34252092+moomoh@users.noreply.github.com>
2023-08-27 18:52:06 +0300

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,114 @@
+# Import required libraries
+from dotenv import load_dotenv
+from itertools import zip_longest
+
+import streamlit as st
+import os
+from streamlit_chat import message
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+
+# Load environment variables
+load_dotenv('openai.env')
+api_key = os.getenv('OPENAI_API_KEY')
+#load_dotenv()
+
+# Set streamlit page configuration
+st.set_page_config(page_title="ChatBot Starter")
+st.title("ChatBot Starter")
+
+# Initialize session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []  # Store AI generated responses
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []  # Store past user inputs
+
+if 'entered_prompt' not in st.session_state:
+    st.session_state['entered_prompt'] = ""  # Store the latest user input
+
+# Initialize the ChatOpenAI model
+chat = ChatOpenAI(
+    openai_api_key=api_key,
+    temperature=0.5,
+    model_name="gpt-3.5-turbo"
+)
+
+
+def build_message_list():
+    """
+    Build a list of messages including system, human and AI messages.
+    """
+    # Start zipped_messages with the SystemMessage
+    zipped_messages = [SystemMessage(
+        content="You are a helpful AI assistant talking with a human. If you do not know an answer, just say 'I don't know', do not make up an answer.")]
+
+    # Zip together the past and generated messages
+    for human_msg, ai_msg in zip_longest(st.session_state['past'], st.session_state['generated']):
+        if human_msg is not None:
+            zipped_messages.append(HumanMessage(
+                content=human_msg))  # Add user messages
+        if ai_msg is not None:
+            zipped_messages.append(
+                AIMessage(content=ai_msg))  # Add AI messages
+
+    return zipped_messages
+
+
+def generate_response():
+    """
+    Generate AI response using the ChatOpenAI model.
+    """
+    # Build the list of messages
+    zipped_messages = build_message_list()
+
+    # Generate response using the chat model
+    ai_response = chat(zipped_messages)
+
+    return ai_response.content
+
+
+# Define function to submit user input
+def submit():
+    # Set entered_prompt to the current value of prompt_input
+    st.session_state.entered_prompt = st.session_state.prompt_input
+    # Clear prompt_input
+    st.session_state.prompt_input = ""
+
+
+# Create a text input for user
+st.text_input('YOU: ', key='prompt_input', on_change=submit)
+
+
+if st.session_state.entered_prompt != "":
+    # Get user query
+    user_query = st.session_state.entered_prompt
+
+    # Append user query to past queries
+    st.session_state.past.append(user_query)
+
+    # Generate response
+    output = generate_response()
+
+    # Append AI response to generated responses
+    st.session_state.generated.append(output)
+
+# Display the chat history
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        # Display AI response
+        message(st.session_state["generated"][i], key=str(i))
+        # Display user message
+        message(st.session_state['past'][i],
+                is_user=True, key=str(i) + '_user')
+
+
+# Add credit
+st.markdown("""
+---
+Made with 🤖 by [Austin Johnson](https://github.com/AustonianAI)""")
