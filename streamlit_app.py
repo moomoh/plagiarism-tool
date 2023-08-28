5dd8d41db5f73806d02df02721fcc14c84b7b10c
moomoh <34252092+moomoh@users.noreply.github.com>
2023-08-29 01:07:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,9 @@ def login_page():
             state["logged_in"] =True
             st.experimental_set_query_params(logged_in=True)
             st.success("Login successful!")
-            login_placeholder.empty()
-            home_page()
+            st.experimental_rerun()
+            #login_placeholder.empty()
+           # home_page()
     return state.logged_in
 
 def logout():
