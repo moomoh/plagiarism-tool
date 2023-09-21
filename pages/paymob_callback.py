a4f2cff238771ea65f16bbba4fe0c23d148f8dd7
moomoh <34252092+moomoh@users.noreply.github.com>
2023-09-22 00:35:28 +0300

    Update paymob_callback.py

--- pages/paymob_callback.py
+++ pages/paymob_callback.py
@@ -2,6 +2,6 @@ import streamlit as st
 import requests
 
 query_params = st.experimental_get_query_params()
-query_dict = {query_params}
+query_dict = dict{query_params}
 st.write(query_params)
 st.write(query_dict)
